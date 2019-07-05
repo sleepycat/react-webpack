0dccdd0554cf23cd92878706292c7566ded6fcc9
Mike Williamson <mike@korora.ca>
2019-07-04 20:35:55 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = ({ mode }) => {
+  return {
+    mode,
+    output: {
+      filename: 'bundle.js',
+    },
+    plugins: [new HtmlWebpackPlugin()],
+  }
+}
