09507010c068237292c319913d759f97fb40c3cf
Mike Williamson <mike.williamson@cds-snc.ca>
2019-07-06 21:40:41 -0400

    Add react-hot-loader
    This commit adds react-hot-loader and it's version of react-🔥-dom so
    that not only does hot loading work, but so does hot reloading with
    hooks.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackRootPlugin = require('html-webpack-root-plugin')
+const { getIfUtils, removeEmpty } = require('webpack-config-utils')
 
 module.exports = ({ mode }) => {
+  const { ifProduction, ifNotProduction } = getIfUtils(mode)
+
   return {
     mode,
     output: {
       filename: 'bundle.js',
     },
+    resolve: removeEmpty({
+      alias: ifNotProduction({
+        'react-dom': '@hot-loader/react-dom',
+      }),
+    }),
     plugins: [new HtmlWebpackPlugin(), new HtmlWebpackRootPlugin()],
     module: {
       rules: [
