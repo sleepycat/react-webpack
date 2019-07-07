7fbfb64d64c2be9542c39c94802594b1d10c7806
Mike Williamson <mike.williamson@cds-snc.ca>
2019-07-06 21:40:41 -0400

    Add react-hot-loader
    This commit adds react-hot-loader and it's version of react-🔥-dom so
    that not only does hot loading work, but so does hot reloading with
    hooks.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { hot } from 'react-hot-loader/root'
 import { say } from './say'
 import React from 'react'
 import { render } from 'react-dom'
 
 say('hello whirled')
 
-const App = () => <main>Hello from React</main>
+const App = hot(() => <main>Hello from React</main>)
 
 render(<App />, document.getElementById('root'))
