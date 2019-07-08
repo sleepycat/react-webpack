aa362e885b559a1f4262f2ee2e74a46c8468962f
Mike Williamson <mike@korora.ca>
2019-07-07 22:16:28 -0400

    Add nonce-based CSP

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackRootPlugin = require('html-webpack-root-plugin')
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin')
 const { getIfUtils, removeEmpty } = require('webpack-config-utils')
 
 module.exports = ({ mode }) => {
@@ -15,7 +16,30 @@ module.exports = ({ mode }) => {
         'react-dom': '@hot-loader/react-dom',
       }),
     }),
-    plugins: [new HtmlWebpackPlugin(), new HtmlWebpackRootPlugin()],
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new HtmlWebpackRootPlugin(),
+      new CspHtmlWebpackPlugin(
+        {
+          'base-uri': "'self'",
+          'object-src': "'none'",
+          'script-src': ["'self'", ifNotProduction("'unsafe-eval'")],
+          'style-src': ["'self'"],
+        },
+        {
+          enabled: true,
+          hashingMethod: 'sha256',
+          hashEnabled: {
+            'script-src': true,
+            'style-src': true,
+          },
+          nonceEnabled: {
+            'script-src': true,
+            'style-src': true,
+          },
+        },
+      ),
+    ],
     devServer: {
       port: 3000,
     },
